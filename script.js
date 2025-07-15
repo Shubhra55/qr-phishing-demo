b52ccc0c358c554f0381f51208d342fafb6d3265
Shubhra55 <shubhrafive@gmail.com>
2025-07-15 21:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("🎓 This is a simulated phishing page for educational purposes.\n\nNo data has been collected.");
+  return false;
+}
